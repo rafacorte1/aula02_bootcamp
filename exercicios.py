f9e203d53a3f8eac7033c06104a7f0f743b6390a
Rafael Corte <rafcorte@gmail.com>
2026-01-21 17:48:35 -0300

    exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -9,3 +9,8 @@ numero1 = int(input('Digite um número: '))
 resto = numero1 % 5 
 print(f'O resto da divisão por 5 é {resto}')
 
+# 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite um número: "))
+resultado = numero1 * numero2
+print(resultado)
