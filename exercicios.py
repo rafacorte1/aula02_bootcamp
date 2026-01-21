c06898eb2236d37fd7dffc1948da989bbde087bd
Rafael Corte <rafcorte@gmail.com>
2026-01-21 18:09:57 -0300

    exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -55,3 +55,55 @@ print(f"{celsius}°C é igual a {fahrenheit}°F")
 raio = float(input("Digite o raio do círculo: "))
 area = 3.14159 * raio ** 2
 print("A área do círculo é:", area)
+
+# 11 - Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+texto = input("Digite um texto: ")
+texto_maiusculas = texto.upper()
+print("Texto em maiúsculas:", texto_maiusculas)
+
+# 12 - Imprimir Nome Completo em Minúsculas
+nome_completo = input("Digite seu nome completo: ")
+nome_minusculas = nome_completo.lower()
+print("Nome em minúsculas:", nome_minusculas)
+
+# 13 - Remover Espaços em Branco de uma Frase
+frase = input("Digite uma frase: ")
+frase_sem_espacos = frase.strip()
+print("Frase sem espaços no início e no final:", frase_sem_espacos)
+
+# 14 - Separar Dia, Mês e Ano de uma Data
+data = input("Digite uma data no formato dd/mm/aaaa: ")
+dia, mes, ano = data.split("/")
+print("Dia:", dia)
+print("Mês:", mes)
+print("Ano:", ano)
+
+# 15 - Concatenar Duas Strings
+parte1 = input("Digite a primeira parte do texto: ")
+parte2 = input("Digite a segunda parte do texto: ")
+texto_concatenado = parte1 + parte2
+print("Texto concatenado:", texto_concatenado)
+
+# 16 - Operador and (E lógico)
+valor1 = True
+valor2 = False
+resultado_and = valor1 and valor2
+print("Resultado do AND lógico:", resultado_and)
+
+# 17 - # Exemplo de entrada
+resultado_or = valor1 or valor2
+print("Resultado do OR lógico:", resultado_or)
+
+# 18 - Operador not (NÃO lógico)
+resultado_not = not valor1
+print("Resultado do NOT lógico:", resultado_not)
+
+# 19 - Operador == (Igualdade)
+num1 = 5
+num2 = 5
+resultado_igualdade = (num1 == num2)
+print("Resultado da igualdade:", resultado_igualdade)
+
+# 20 - Operador != (Diferença)
+resultado_diferenca = (num1 != num2)
+print("Resultado da diferença:", resultado_diferenca)
