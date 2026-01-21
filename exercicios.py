65cb40df254aa15ac5567a1c6889e625f5cb798c
Rafael Corte <rafcorte@gmail.com>
2026-01-21 17:44:13 -0300

    exercicios.py

---
+++ exercicios.py
@@ -0,0 +1,6 @@
+# 1 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite um número: "))
+resultado = numero1 + numero2
+print(resultado)
+
